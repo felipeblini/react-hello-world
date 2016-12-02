3d9c7fe14fd1483370d0d95b88fc61c0034f0720
Felipe blini <Felipe.blini@gmail.com>
2016-12-02 20:52:55 -0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var reactDom = require('react-dom');
+
+// código HTML escrito em JSX e jogado no elemento ui com id app
+reactDom.render('<h1>Hello World</h1>', document.getElementById('app'));
